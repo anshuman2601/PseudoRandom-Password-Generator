0d2057da00a249ab9b5b015641c773b84fb53b0e
Anshuman S <anshuman2601@gmail.com>
2021-02-14 16:33:44 -0600

    Added the src file

---
+++ password.py
@@ -0,0 +1,7 @@
+import string
+from random import *
+
+chars = string.ascii_letters + string.punctuation + string.digits
+
+password = "".join(choice(chars) for x in range(randint(8, 20)))
+print (password)
